290d9879c8f4c28e7b175de4faa7ceef69328e5d
MohanKumar <MohanKumar@DESKTOP-N60UCLJ>
2023-07-28 15:49:57 +0530

    commit

---
+++ Java_Program/src/com/cg/dayone/Typecasting.java
@@ -0,0 +1,13 @@
+package com.cg.dayone;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		int a = 67;
+		System.out.println(a);	
+		float b = a;
+		System.out.println(b);
+		long c = a;
+		System.out.println(c);
+	}
+}
